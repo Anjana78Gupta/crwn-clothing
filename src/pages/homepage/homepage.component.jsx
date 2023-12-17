80f7f12ee5d22e65abf3d8366596226bbbb62e15
Anjana <anjana2003anajna@gmail.com>
2023-12-18 01:18:27 +0530

    update our menu-itmes to navigate withRout HOC

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import './homepage.styles.scss';
 import MenuItem from "../../components/menu-item/menu-item.component";
 import Directory from "../../components/directory/directory.component";
+
+
+
 const HomePage = () => (
     <div className="homepage">
         <Directory />
